8b15b52006ff0991a67036ea804150efba4eb177
yarik <yariks@i.ua>
2018-10-27 10:17:43 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+public class Main {
+
+    public static void main(String[] args)  {
+        Pattern.compile("^[a|b].*@gmail\\.com.*a$")
+    }}
